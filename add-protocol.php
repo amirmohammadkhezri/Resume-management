efe9b533f0cfccebb92cf80149afcda2cc4d8795
amirmohammadkhezri <49806187+amirmohammadkhezri@users.noreply.github.com>
2020-01-25 23:41:08 +0330

    mix html file

--- add-protocol.php
+++ add-protocol.php
@@ -1,7 +1,6 @@
 <?php
  include_once 'user_functions.php';
     include_once 'functions.php';
-
 if(isset($_POST['btn'])){
 	$data=$_POST['frm'];
 	$dir='uploader/';
@@ -16,37 +15,23 @@ if(isset($_POST['btn'])){
 	$to=$dir.$new_name1."/".$new_name;
 	move_uploaded_file($from,$to);
 	addmenu($data,$to);
-
 	header("location:add_list_protocol.php");
 }
-?>
-<?php
-
-
 $connections=config();
 $sqls="SELECT * FROM folder ORDER BY id DESC";
 $results=mysqli_query($connections,$sqls);
-
 ?>
-
-
-
 
 <!DOCTYPE html>
 <html lang="en" dir="rtl">
-
 <head>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<meta name="description" content="Neon Admin Panel" />
 	<meta name="author" content="" />
-
 	<link rel="icon" href="assets/images/favicon.ico">
-
 	<title>سیستم مدیریت رزومه ها</title>
-
 	<link rel="stylesheet" href="assets/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css">
 	<link rel="stylesheet" href="assets/css/font-icons/entypo/css/entypo.css">
 	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic">
@@ -55,14 +40,10 @@ $results=mysqli_query($connections,$sqls);
 	<link rel="stylesheet" href="assets/css/neon-theme.css">
 	<link rel="stylesheet" href="assets/css/neon-forms.css">
 	<link rel="stylesheet" href="assets/css/neon-rtl.css">
-
-
 	<script src="assets/js/jquery-1.11.3.min.js"></script>
-
 </head>
 
 <body class="page-body  page-fade" data-url="http://neon.dev">
-
 	<div class="page-container">
 		<!-- add class "sidebar-collapsed" to close sidebar by default, "chat-visible" to make chat appear always -->
 		<!-- side bar delete -->
@@ -90,45 +71,26 @@ $results=mysqli_query($connections,$sqls);
 										</div>
 									</div>
 									<div class="col-sm-8">
-
-									
-									
-
 									<div class="col-sm-4">
-
 										<div class="form-group">
 											<label class="col-sm-12 control-label">شماره تماس</label>
-
 											<div class="col-sm-12">
 												<input type="text" value="" name="frm[tel]" class="form-control" placeholder="PhoneNumber" required>
-													
 											</div>
 										</div>
-
 									</div>
-
-
-
 									<div class="col-sm-4">
-
 										<div class="form-group">
 											<label class="col-sm-12 control-label">جنسیت</label>
-
 											<div class="col-sm-12">
 											<select name="frm[gender]" style="width: 150px";>
 												<option value="مرد" selected>مرد</option>
 																<option value="زن">زن</option>
 													</select>
-													
 											</div>
 										</div>
-
 									</div>
-									
-									
-									
 									<div class="col-sm-4">
-									
 									<div class="form-group">
 									<label class="col-sm-12 control-label">شماره آگهی</label>
 									<div class="col-sm-12">
@@ -144,7 +106,6 @@ $results=mysqli_query($connections,$sqls);
 											</div>
 									</div>
 									</div>
-
 								</div>
 								</div>
 								<div class="alert alert-info" role="alert">رزومه کاری خود را پیوست کنید و در صورت لزوم میتوانید توضیحاتی در رابطه با رزومه کاری خود ارسال کنید</div>
@@ -163,11 +124,7 @@ $results=mysqli_query($connections,$sqls);
 										<div class="alert alert-warning" role="alert">
 										<h4 class="alert-heading">هشدار !</h4>
 										<p>امکان ویرایش رزومه ارسالی وجود ندارد</p>
-										
 										<p class="mb-0">لطفا قبل از ارسال رزومه کاری خود ورودی های خود را چک کنید</p>
-
-	
-
 										</div>
 												<div class="form-group">
 													<label class="col-sm-12 control-label">&nbsp;</label>
@@ -188,26 +145,13 @@ $results=mysqli_query($connections,$sqls);
 									</div>
 								</div>
 								</div>
-
 							</form>
-
 						</div>
-
 					</div>
-
 				</div>
 			</div>
-
-			
-
-
-
 		</div>
-
-
 	</div>
-
-
 	<!-- Imported styles on this page -->
 	<link rel="stylesheet" href="assets/js/jvectormap/jquery-jvectormap-1.2.2.css">
 	<link rel="stylesheet" href="assets/js/rickshaw/rickshaw.min.css">
@@ -220,32 +164,15 @@ $results=mysqli_query($connections,$sqls);
 	<link rel="stylesheet" href="assets/js/icheck/skins/flat/_all.css">
 	<link rel="stylesheet" href="assets/js/icheck/skins/futurico/futurico.css">
 	<link rel="stylesheet" href="assets/js/icheck/skins/polaris/polaris.css">
-
-	<!-- Custom CSS File -->
 	<link rel="stylesheet" href="assets/css/custom.css">
-
 	<!-- Bottom scripts (common) -->
-
 	<script src="assets/js/bootstrap.js"></script>
 	<script src="assets/js/select2/select2.min.js"></script>
-	
-
-
-	<!-- Imported scripts on this page -->
-	
-
-	
 	<script src="ckeditor/ckeditor.js"></script>
         <script src="ckeditor/adapters/jquery.js"></script>
-
-
 	<!-- JavaScripts initializations and stuff -->
 	<script src="assets/js/neon-custom.js"></script>
-
-
 	<!-- Demo Settings -->
 	<script src="assets/js/neon-demo.js"></script>
-
 </body>
-
 </html>
--- addFolder.php
+++ addFolder.php
@@ -3,19 +3,15 @@ include_once 'user_functions.php';
 $connection = config();
 $sql = "SELECT * FROM folder ORDER BY id DESC";
 $result = mysqli_query($connection, $sql);
-
 include_once 'user_functions.php';
 include_once 'functions.php';
 if (!isset($_SESSION['username'])) {
 	header("location:login.php");
 }
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en" dir="rtl">
-
 <head>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
@@ -23,11 +19,8 @@ if (!isset($_SESSION['username'])) {
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<meta name="description" content="Neon Admin Panel" />
 	<meta name="author" content="" />
-
 	<link rel="icon" href="assets/images/favicon.ico">
-
 	<title>سیستم مدیریت رزومه ها</title>
-
 	<link rel="stylesheet" href="assets/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css">
 	<link rel="stylesheet" href="assets/css/font-icons/entypo/css/entypo.css">
 	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic">
@@ -36,62 +29,37 @@ if (!isset($_SESSION['username'])) {
 	<link rel="stylesheet" href="assets/css/neon-theme.css">
 	<link rel="stylesheet" href="assets/css/neon-forms.css">
 	<link rel="stylesheet" href="assets/css/neon-rtl.css">
-
-
 	<script src="assets/js/jquery-1.11.3.min.js"></script>
-
-
 </head>
 
 <body class="page-body" data-url="http://neon.dev">
-
 	<div class="page-container">
 		<!-- add class "sidebar-collapsed" to close sidebar by default, "chat-visible" to make chat appear always -->
-
 		<div class="sidebar-menu">
-
 			<div class="sidebar-menu-inner">
-
 				<header class="logo-env">
-
 					<!-- logo -->
 					<div class="logo">
-
 						<a href="index.php">
-
 							<img src="assets/images/logo@2x.png" width="150" alt="" />
 						</a>
 					</div>
 					<br>
-
 					<br>
-
 					<!-- logo collapse icon -->
 					<div class="sidebar-collapse">
 						<br>
-
 						<!-- add class "with-animation" if you want sidebar to have animation during expanding/collapsing transition -->
-
 						</a>
-
 					</div>
-
-
 					<!-- open/close menu icon (do not remove if you want to enable menu on mobile devices) -->
-
 					<div class="sidebar-mobile-menu visible-xs">
 						<a href="#" class="with-animation">
 							<!-- add class "with-animation" to support animation -->
-
 							<i class="entypo-menu"></i>
-
 						</a>
-
 					</div>
-
 				</header>
-
-
 				<ul id="main-menu" class="main-menu">
 					<li class="active">
 						<a href="index.php">
@@ -100,33 +68,20 @@ if (!isset($_SESSION['username'])) {
 						</a>
 					</li>
 					<li class="has-sub root-level">
-
 					<li>
 						<a href="addFolder.php">
 							<span class="title">مدیریت پوشه ها</span>
 						</a>
 					</li>
-
 					</li>
-
 					</li>
 				</ul>
 			</div>
-
-
-
 		</div>
-
 		<div class="main-content">
-
 			<div class="row">
-
 				<!-- Profile Info and Notifications -->
 				<div class="col-md-6 col-sm-8 clearfix">
-
-
-
-
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
 						<img src="assets/images/thumb-1@2x.png" alt="" class="img-circle" width="44" />
 						<?php
@@ -135,25 +90,17 @@ if (!isset($_SESSION['username'])) {
 				</div>
 				<!-- Raw Links -->
 				<div class="col-md-6 col-sm-4 clearfix hidden-xs">
-
 					<ul class="list-inline links-list pull-right">
-
-
 						<li>
 							<a href="login.php">
 								خروج <i class="entypo-logout right"></i>
 							</a>
 						</li>
 					</ul>
-
 				</div>
-
 			</div>
-
 			<hr />
-
 			<br />
-
 			<script type="text/javascript">
 				jQuery(document).ready(function($) {
 					var $table1 = jQuery('#table-1');
@@ -177,34 +124,27 @@ if (!isset($_SESSION['username'])) {
 					button.removeAttr('enable');
 				}, 1000);;
 			</script>
-
 			<table class="table table-bordered datatable" id="table-1">
 				<thead>
 					<a href="newfolder.php">
-
 						<button type="button" class="btn btn-success"><i class="entypo-folder"> </i>ایجاد پوشه</button>
 					</a>
 					<br />
 					<br />
 					<br />
 					<tr>
-
 						<th>ردیف</th>
 						<th data-hide="phone">عنوان</th>
 						<th>شماره آگهی</th>
-
-
 						<th data-hide="phone,tablet">حذف</th>
 						<th data-hide="phone,tablet">بروزرسانی لینک ها</th>
 					</tr>
 				</thead>
 				<tbody>
-
 					<?php
 					while ($row = mysqli_fetch_assoc($result)) {
 					?>
 						<tr class="odd gradeX">
-
 							<td><?php echo $row['id']; ?></td>
 							<td>
 								<a href="list_protocol.php?secret=<?php echo $row['hashing']; ?>"><?php echo '<i class="entypo-folder" style="color: darkorange;"></i>' . '&nbsp;' . $row['title']; ?></a>
@@ -226,23 +166,16 @@ if (!isset($_SESSION['username'])) {
 								<!-- id="urlCopied"  -->
 								<!-- <textarea onclick="myFunction()" class="btn btn-blue" id="urlCopied" rows="1" cols="30"></textarea> -->
 							</td>
-
 							<td class="text-center">
 								<a href="generate.php?id=<?php echo $row['hashing']; ?>" onclick="return confirm('میخواد لینک جدید بسازید ؟ ')">
 
 									<button type="button" id="send" class="btn btn-secondary  btn-xs" disabled>دریافت لینک جدید +</button>
 								</a>
-
 							<?php
 						}
-
 							?>
-
 							<!-- id="urlCopied"  -->
 							<input type="hidden" value="<?php echo (md5(rand(1000, 7800787))) ?>" name="frm[hashing]">
-
-
-							<!-- <textarea onclick="myFunction()" class="btn btn-blue" id="urlCopied" rows="1" cols="30"></textarea> -->
 							</td>
 						</tr>
 
@@ -252,35 +185,16 @@ if (!isset($_SESSION['username'])) {
 				</tbody>
 			</table>
 		</div>
-
-
-
 		<!-- Imported styles on this page -->
 		<link rel="stylesheet" href="assets/js/datatables/datatables.css">
-
-
 		<!-- Custom CSS File -->
 		<link rel="stylesheet" href="assets/css/custom.css">
-
 		<!-- Bottom scripts (common) -->
-
 		<script src="assets/js/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js"></script>
 		<script src="assets/js/bootstrap.js"></script>
-
-
-
 		<!-- Imported scripts on this page -->
 		<script src="assets/js/datatables/datatables.js"></script>
-
-
-
 		<!-- JavaScripts initializations and stuff -->
 		<script src="assets/js/neon-custom.js"></script>
-
-
-		<!-- Demo Settings -->
-
-
 </body>
-
 </html>
